e1ef98fa48c93bb1dd966463502ecbedd6929b49
shimna <chimmu.p@gmail.com>
2023-07-03 15:56:13 +1000

    created function for displaying movie details

--- Assets/js/moviedetails.js
+++ Assets/js/moviedetails.js
@@ -8,8 +8,8 @@ var getMovieById = function (movieId) {
             if (response.ok) {
 
                 response.json().then(function (data) {
+                    displayMovieById(data);
 
-                    
                 });
             } else {
                 alert('Error: ' + response.statusText);
@@ -20,4 +20,23 @@ var getMovieById = function (movieId) {
         });
 };
 getMovieById(movieId);
+var displayMovieById = function (movies) {
+    console.log(movies);
+    if (movies.length === 0) {
+        return;
+    }
+    var movieId = movies.id;
+    var movieTitle = movies.title;
+    var movieReleaseDate = movies.release_date;
+    var releaseDate = dayjs(movieReleaseDate).format('D MMMM YYYY');
+    var moviePosterPath = 'https://image.tmdb.org/t/p/w500//' + movies.poster_path;
+    console.log(movieTitle);
+    console.log(releaseDate);
+    console.log(moviePosterPath);
+    for (var i = 0; i < 3; i++) {
+        console.log(movies.credits.cast[i].name);
+    }
+    var overview = movies.overview;
+    console.log(overview);
+};
 
