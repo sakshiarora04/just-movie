d95676ad69b234d0e275e2ce11d2530f46ade28b
shimna <chimmu.p@gmail.com>
2023-07-04 20:26:35 +1000

    added a modal for validation

--- Assets/js/moviedetails.js
+++ Assets/js/moviedetails.js
@@ -1,11 +1,11 @@
 // var movieId = 343611;
 var movieId = 550;
-var apiKey = 'api_key=533313cc880a2148c77843e769ec1a97';
+var apiKey = '533313cc880a2148c77843e769ec1a97';
 
 // Fetch movie details by id
 var getMovieById = function (movieId) {
-    
-    var apiUrl = 'https://api.themoviedb.org/3/movie/' + movieId + '?' + apiKey + '&language=en-US&append_to_response=credits';
+
+    var apiUrl = 'https://api.themoviedb.org/3/movie/' + movieId + '?api_key=' + apiKey + '&language=en-US&append_to_response=credits';
     fetch(apiUrl, { cache: 'reload' })
         .then(function (response) {
             if (response.ok) {
@@ -26,8 +26,8 @@ getMovieById(movieId);
 
 // Displays movie details 
 var displayMovieDetails = function (movies) {
-    // console.log(movies);
     if (movies.length === 0) {
+        $('#exampleModal1').foundation('open');
         return;
     }
     var movieId = movies.id;
@@ -36,7 +36,7 @@ var displayMovieDetails = function (movies) {
     var releaseYear = dayjs(movieReleaseDate).format('YYYY');
     var movieTitle = movies.title + ' (' + releaseYear + ')';
     var moviePosterPath = 'https://image.tmdb.org/t/p/w500///' + movies.poster_path;
-    var imageEl=$('#imgId').attr('src',moviePosterPath);
+    var imageEl = $('#imgId').attr('src', moviePosterPath);
     var genre = '';
     for (var i = 0; i < movies.genres.length; i++) {
         if (genre === '') {
@@ -54,7 +54,7 @@ var displayMovieDetails = function (movies) {
         if (movies.credits.crew[i].job == "Writer") {
             var writer = movies.credits.crew[i].name;
         }
-    }   
+    }
     var overview = movies.overview;
     var runtime = Math.floor(118 / 60) + 'h    ' + 118 % 60 + 'min';
     var userRating = movies.vote_average.toFixed(1);
@@ -69,58 +69,56 @@ var displayMovieDetails = function (movies) {
     titleEl.append($('<p>').css('display', 'block').append($('<strong>').text('Release Date : ')).append($('<p>').css('display', 'inline-block').text(releaseDate)));
     titleEl.append($('<p>').css('display', 'block').append($('<strong>').text('Director : ')).append($('<p>').css('display', 'inline-block').text(director)));
     titleEl.append($('<p>').css('display', 'block').append($('<strong>').text('Overview : ')).append($('<p>').css('display', 'inline').text(overview)));
-    
+
     var castEl = $('#cast-details');
     for (var i = 0; i < movies.credits.cast.length; i++) {
-    if(movies.credits.cast[i].profile_path!=null)
-    {
-    var castProfilePath='https://image.tmdb.org/t/p/w500///' +movies.credits.cast[i].profile_path;
-    var cardEl=$('<div class="card small-" style="height:300px; width:170px ; background-color:white"></div>');
-    var castImageEl=$('<img style="height:200px; width:170px">').attr("src",castProfilePath);
-    var cardSectionEl=$('<div class="card-section" style="background-color:white"></div>');
-    var castName=$('<p>').append($('<strong>').text(movies.credits.cast[i].name));
-    var castRole=$('<p>').text(movies.credits.cast[i].character);
-    cardEl.append(castImageEl);
-    cardSectionEl.append(castName).append(castRole);
-    cardEl.append(cardSectionEl);
+        if (movies.credits.cast[i].profile_path != null) {
+            var castProfilePath = 'https://image.tmdb.org/t/p/w500///' + movies.credits.cast[i].profile_path;
+            var cardEl = $('<div class="card small-" style="height:300px; width:170px ; background-color:white"></div>');
+            var castImageEl = $('<img style="height:200px; width:170px">').attr("src", castProfilePath);
+            var cardSectionEl = $('<div class="card-section" style="background-color:white"></div>');
+            var castName = $('<p>').append($('<strong>').text(movies.credits.cast[i].name));
+            var castRole = $('<p>').text(movies.credits.cast[i].character);
+            cardEl.append(castImageEl);
+            cardSectionEl.append(castName).append(castRole);
+            cardEl.append(cardSectionEl);
 
-    castEl.append(cardEl);
+            castEl.append(cardEl);
+        }
     }
-    }   
 
 };
 //function to fetch reviews by movie id
 var getReviewsByMovieId = function (movieId) {
-    var apiUrl = 'https://api.themoviedb.org/3/movie/' + movieId + '/reviews?' + apiKey;
+    var apiUrl = 'https://api.themoviedb.org/3/movie/' + movieId + '/reviews?api_key=' + apiKey;
     fetch(apiUrl, { cache: 'reload' })
-    .then(function (response) {
-        if (response.ok) {
+        .then(function (response) {
+            if (response.ok) {
 
-            response.json().then(function (data) {
-                console.log(data.results);
-                displayReviews(data.results);
-            });
-        } else {
-            // alert('Error: ' + response.statusText);
-        }
-    })
-    .catch(function (error) {
-        alert('Unable to connect to Fetch');
-    });
+                response.json().then(function (data) {
+                    console.log(data.results);
+                    displayReviews(data.results);
+                });
+            } else {
+                // alert('Error: ' + response.statusText);
+            }
+        })
+        .catch(function (error) {
+            alert('Unable to connect to Fetch');
+        });
 };
 getReviewsByMovieId(movieId);
-function displayReviews(reviews)
-{
+function displayReviews(reviews) {
     for (var i = 0; i < reviews.length; i++) {
-        if(i===0){
-        var review=reviews[0].content;
-        var author=reviews[0].author;
-        var writtenDate=dayjs(reviews[2].created_at).format('DD-MM-YY');
-        // var writtenDate=dayjs(reviews[2].created_at).format('D MMMM YYYY');
-        var titleEl = $('#movie-reviews');
-        titleEl.append($('<h5>').text('A review written by '+author));
-        titleEl.append($('<p>').text('Written by '+author+'on '+writtenDate));
-        titleEl.append($('<p>').css('display', 'block').append($('<p>').css('display', 'inline').text(review)));
+        if (i === 0) {
+            var review = reviews[0].content;
+            var author = reviews[0].author;
+            var writtenDate = dayjs(reviews[2].created_at).format('DD-MM-YY');
+            // var writtenDate=dayjs(reviews[2].created_at).format('D MMMM YYYY');
+            var titleEl = $('#movie-reviews');
+            titleEl.append($('<h5>').text('A review written by ' + author));
+            titleEl.append($('<p>').text('Written by ' + author + 'on ' + writtenDate));
+            titleEl.append($('<p>').css('display', 'block').append($('<p>').css('display', 'inline').text(review)));
         }
         console.log(writtenDate);
         // console.log(author);
@@ -129,6 +127,6 @@ function displayReviews(reviews)
     }
 
 }
-$("#play-review").on("click",function(){
+$("#play-review").on("click", function () {
     responsiveVoice.speak('David Fincher’s new film, Mank, is coming soon on Netflix, released six years after his latest');
 });
