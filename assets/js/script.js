0559539813b349c0a8f7bd6f758e973856e9e70f
Sakshi Arora <sakshiarora245@gmail.com>
2023-07-07 06:46:12 +1000

    fix slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,75 +1,101 @@
-const sliders = document.getElementsByClassName("slide-content");
-var swipers=[];
-
-for (let slide of sliders) {
-  
-  const nextEl = slide.getElementsByClassName("swiper-button-next")[0];
-  const prevEl = slide.getElementsByClassName("swiper-button-prev")[0];
- 
-  var swiper = new Swiper(slide, {
-    speed: 300,
-    slidesPerView: 3,
-    spaceBetween: 15,
-    loop: true,
-    calculateHeight: true,
-    centerSlide: true,
-    fade: true,
-    grabCursor: true,
-    preloadImages: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
-    },
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      520: {
-        slidesPerView: 2,
-      },
-      950: {
-        slidesPerView: 3,
-      },
-    },
-    navigation: {
-      nextEl,
-      prevEl,
-    },
-  });
-
-  swipers.push(swiper);
-  console.log(swiper);
-
-}
-
 var apiKey = "dd82df3a677360e4edaa486372b0e8b4";
-function getMostSearchedResults() {
-  var mostSearchedUrl =
-    "https://api.themoviedb.org/3/trending/movie/week?api_key=" + apiKey;
-  fetch(mostSearchedUrl)
-    .then(function (response) {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then(function (data) {
-      if (!data.results.length) {
-        $(".slide-content").html("No results found");
-      } else {
-        console.log("search")
-        renderMostSearchedResults(data);
+var searchFormEl = $('#search-form');
+var calloutEl = $('#errorMessage');
+var swiper1 = new Swiper('#slide-rated', {
+  speed: 300,
+  slidesPerView: 3,
+  spaceBetween: 15,
+  loop: true,
+  calculateHeight: true,
+  centerSlide: true,
+  fade: true,
+  grabCursor: true,
+  preloadImages: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+  navigation: {
+    nextEl:'#button-rated-next',
+    prevEl:'#button-rated-prev',
+  },
+});
+var swiper2 = new Swiper('#slide-search', {
+  speed: 300,
+  slidesPerView: 3,
+  spaceBetween: 15,
+  loop: true,
+  calculateHeight: true,
+  centerSlide: true,
+  fade: true,
+  grabCursor: true,
+  preloadImages: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+  navigation: {
+    nextEl:'#button-search-next',
+    prevEl:'#button-search-prev',
+  },
+});
 
-      }
-    })
-    .catch((error) => {
-      console.error("Fetch error:", error);
-      $(".slide-content").html(
-        "<h3>Error occurred while fetching search results</h3>"
-      );
-    });
-}
+var swiper3 = new Swiper('#slide-recent', {
+  speed: 300,
+  slidesPerView: 3,
+  spaceBetween: 15,
+  loop: true,
+  calculateHeight: true,
+  centerSlide: true,
+  fade: true,
+  grabCursor: true,
+  preloadImages: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    520: {
+      slidesPerView: 2,
+    },
+    950: {
+      slidesPerView: 3,
+    },
+  },
+  navigation: {
+    nextEl:'#button-recent-next',
+    prevEl:'#button-recent-prev',
+  },
+});
+
 function getRatedResults() {
   var mostRatedUrl =
     "https://api.themoviedb.org/3/movie/top_rated?api_key=" + apiKey;
@@ -96,6 +122,33 @@ function getRatedResults() {
       );
     });
 }
+function getMostSearchedResults() {
+  var mostSearchedUrl =
+    "https://api.themoviedb.org/3/trending/movie/week?api_key=" + apiKey;
+  fetch(mostSearchedUrl)
+    .then(function (response) {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then(function (data) {
+      if (!data.results.length) {
+        $(".slide-content").html("No results found");
+      } else {
+        console.log("search")
+        renderMostSearchedResults(data);
+
+      }
+    })
+    .catch((error) => {
+      console.error("Fetch error:", error);
+      $(".slide-content").html(
+        "<h3>Error occurred while fetching search results</h3>"
+      );
+    });
+}
+
 function getRecentReleasesResults() {
   var mostRecentUrl =
     "https://api.themoviedb.org/3/discover/movie?&with_original_language=en&primary_release_date.gte=2023-01-01&primary_release_date.lte=2023-07-03&sort_by=primary_release_date.desc&api_key=" + apiKey;
@@ -110,7 +163,7 @@ function getRecentReleasesResults() {
       if (!data.results.length) {
         $(".slide-content").html("No results found");
       } else {
-        console.log("search")
+        console.log("recent")
         renderRecentReleasesResults(data);
 
       }
@@ -123,21 +176,9 @@ function getRecentReleasesResults() {
     });
 }
 function init() {
-  getMostSearchedResults();
   getRatedResults();
+  getMostSearchedResults();  
   getRecentReleasesResults();
-}
-
-function renderMostSearchedResults(obj) {
-  //loop to print cards inside carousal
-  for (var i = 0; i < obj.results.length; i++) {
-    var card = printResults(obj.results[i]);
-    var slideShow = $('<div class="card-search swiper-slide"></div>');
-    slideShow.append(card);
-    swipers[1].appendSlide(slideShow);
-    swipers[1].update();
-  
-  }
 }
 function renderRatedResults(obj) {
   //loop to print 4 cards inside carousal
@@ -145,20 +186,32 @@ function renderRatedResults(obj) {
     var card = printResults(obj.results[i]);
     var slideShow = $('<div class="card-search swiper-slide"></div>');
     slideShow.append(card);
-    swipers[0].appendSlide(slideShow);
-    swipers[0].update();
+    swiper1.appendSlide(slideShow);
+    swiper1.update();
     
   }
 }
+function renderMostSearchedResults(obj) {
+  //loop to print cards inside carousal
+  for (var i = 0; i < obj.results.length; i++) {
+    var card = printResults(obj.results[i]);
+    var slideShow = $('<div class="card-search swiper-slide"></div>');
+    slideShow.append(card);   
+    swiper2.appendSlide(slideShow);
+    swiper2.update();
+  
+  }
+}
+
 function renderRecentReleasesResults(obj) {
   //loop to print 4 cards inside carousal
   for (var i = 0; i < obj.results.length; i++) {
     var card = printResults(obj.results[i]);
     var slideShow = $('<div class="card-search swiper-slide"></div>');
     slideShow.append(card);
-    swipers[2].appendSlide(slideShow);
-    swipers[2].update();
-    
+    swiper3.appendSlide(slideShow);
+    swiper3.update();
+    console.log(card)    
   }
 }
 function printResults(result) {
@@ -199,8 +252,7 @@ else{
 //   window.location = "check.html";
 //   });
 init();
-var searchFormEl = $('#search-form')
-var calloutEl = $('#errorMessage')
+
 
 function handleSearchFormSubmit(event) {
     event.preventDefault();
@@ -210,16 +262,14 @@ function handleSearchFormSubmit(event) {
     if (!searchInputVal) {
         console.error('You need a search input value!');
         $('#no-input').foundation('open'); // Show the popup
-
         return;
     }
 
     var query = searchInputVal.replace(/\s/g, '+');
-    // console.log(query)
     var queryString = './display-search.html?q=' + query
 
 
-    location.assign(queryString)
+    location.assign(queryString);
 }
 
-searchFormEl.on('submit', handleSearchFormSubmit)
+searchFormEl.on('submit', handleSearchFormSubmit);
